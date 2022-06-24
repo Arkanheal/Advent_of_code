a7a7bbf6f2957bb1597698283314e2e731de5cc6
Arkan <arkan@DESKTOP-6FF5JF3.localdomain>
2022-06-24 22:51:43 +0200

    feat: day 2 A (not clean though)

--- aoc_2020_2/aoc_2020_2_a/src/main.rs
+++ aoc_2020_2/aoc_2020_2_a/src/main.rs
@@ -2,8 +2,19 @@ use std::fs;
 use regex::Regex;
 
 fn main() {
-    let _re = Regex::new(r"^(\d+?)-(\d+?)\s([a-z]):\s([a-z])+?").unwrap();
-    fs::read_to_string("../input.txt").unwrap()
+    let re = Regex::new(r"^(\d+?)-(\d+?)\s([a-z]):\s([a-z]+?)$").unwrap();
+    let lines : Vec<String> = fs::read_to_string("../input.txt").unwrap()
         .lines()
-        .map(|line| line.parse::<String>().unwrap());
+        .map(|line| line.parse::<String>().unwrap()).collect();
+    let mut i = 0;
+    for line in lines.iter() {
+        for capture in re.captures_iter(line) {
+            let re_in = Regex::new(format!(r"{}", &capture[3]).as_str()).unwrap();
+            if &capture[1].parse::<usize>().unwrap() <= &re_in.captures_iter(&capture[4]).count() &&
+                &capture[2].parse::<usize>().unwrap() >= &re_in.captures_iter(&capture[4]).count() {
+                i += 1;
+            }
+        }
+    }
+    print!("{}", i);
 }

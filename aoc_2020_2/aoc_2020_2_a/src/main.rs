cd96f3ec444fad62f02e37f94b2169727ca1330a
Arkan <arkan@DESKTOP-6FF5JF3.localdomain>
2022-06-25 18:05:23 +0200

    refactor: Usage of matches instead of second regex

--- aoc_2020_2/aoc_2020_2_a/src/main.rs
+++ aoc_2020_2/aoc_2020_2_a/src/main.rs
@@ -1,20 +1,13 @@
-use std::fs;
 use regex::Regex;
 
 fn main() {
     let re = Regex::new(r"^(\d+?)-(\d+?)\s([a-z]):\s([a-z]+?)$").unwrap();
-    let lines : Vec<String> = fs::read_to_string("../input.txt").unwrap()
+    println!("{}",
+    include_str!("../../input.txt")
         .lines()
-        .map(|line| line.parse::<String>().unwrap()).collect();
-    let mut i = 0;
-    for line in lines.iter() {
-        for capture in re.captures_iter(line) {
-            let re_in = Regex::new(format!(r"{}", &capture[3]).as_str()).unwrap();
-            if &capture[1].parse::<usize>().unwrap() <= &re_in.captures_iter(&capture[4]).count() &&
-                &capture[2].parse::<usize>().unwrap() >= &re_in.captures_iter(&capture[4]).count() {
-                i += 1;
-            }
-        }
-    }
-    print!("{}", i);
+        .map(|line| re.captures(line).unwrap())
+        .filter(|line| {
+            let len = line[4].matches(&line[3]).count();
+            len >= line[1].parse().unwrap() && len <= line[2].parse().unwrap()
+        }).count());
 }
